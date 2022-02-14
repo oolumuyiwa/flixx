609fb15404b4d5cf37be6f29ed8b5c2080d8b5c1
oolumuyiwa <oolumuyiwa@vassar.edu>
2022-02-13 22:59:44 -0500

    Updated Flixx

---
+++ Flixx/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flixx
+//
+//  Created by Oluwatomiwa Olumuyiwa on 2/7/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+  
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
--- Flixx/ViewController.swift
+++ Flixx/ViewController.swift
@@ -69,6 +69,19 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
 }
 
